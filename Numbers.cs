61edac357e6fde6adec7b7e173c3bdd37dd899a1
greedy2119 <greedygreedy2119@gmail.com>
2021-03-31 13:36:47 +0700

    Latihan Overloadin Menthod

---
+++ Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanOverloadingMenthod
+{
+    class Numbers
+    {
+
+        public int FindMinimum(int number1, int number2)
+        {
+            int number = Math.Min(number1, number2);
+            return number;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int number = (int)Math.Min(number1, Math.Min(number2, number3));
+            return number;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            int number = Math.Max(number1, number2);
+            return number;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int number = (int)Math.Max(number1, Math.Max(number2, number3));
+            return number;
+        }
+    }
+}
